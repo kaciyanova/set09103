9f5541f9c9e425d0b5ed8db1f95ab822d90f8ba8
Kaci Yanova <kaciyanova@gmail.com>
2019-10-03 01:21:17 +0100

    swap file deleted everything's Fine (-:

--- hello.py
+++ hello.py
@@ -16,9 +16,9 @@ def root():
 
 @app.route("/static-example/img")
 def static_example_img():
-    start= '<img src="'
+    start='<img src="'
     url = url_for('static',filename='vmask.jpg')
-    end = '">'
+    end ='">'
     return start+url+end,200
 
 @app.route("/hello/")
